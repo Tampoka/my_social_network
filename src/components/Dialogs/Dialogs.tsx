0e108c3c2d05674e4dc9211059f66c5a60be38ea
Tampoka <goal.thanks@gmail.com>
2021-08-07 11:47:50 +0900

    moved DialogItem component to DialogItem folder

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import s from './Dialogs.module.css'
-
-type DialogItemPropsType = {
-    name: string
-    id: number
-}
-const DialogItem = (props: DialogItemPropsType) => {
-    let path = "/dialogs/" + props.id
-    return (
-        <div className={s.dialog}>
-            <NavLink to={path} activeClassName={s.active}>{props.name}</NavLink>
-        </div>
-    )
-}
+import DialogItem from "./DialogItem/DialogItem";
 
 type MessagePropsType = {
     message: string
