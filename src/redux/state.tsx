7c69442895ca90a2c96453bf9074c337b7e2ff81
Tampoka <goal.thanks@gmail.com>
2021-08-07 22:07:03 +0900

    changed state

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,26 +1,30 @@
 let state = {
-    posts: [
-        {id: 1, message: "How are you?", likesCount: 19},
-        {id: 2, message: "Where have you been?", likesCount: 3},
-        {id: 3, message: "Pics from our party yesterday", likesCount: 8},
-        {id: 4, message: "How important is to be proactive?", likesCount: 27},
-        {id: 5, message: "What are your goals for today, for this week?", likesCount: 54},
-    ],
-    dialogs: [
-    {id: 1, name: "Dimych"},
-    {id: 2, name: "Victor"},
-    {id: 3, name: "Bob"},
-    {id: 4, name: "John"},
-    {id: 5, name: "Anna"},
-    {id: 6, name: "Dennis"},
-],
-    messages: [
-    {id: 1, message: "How are you?"},
-    {id: 2, message: "Are you busy now?"},
-    {id: 3, message: "Today I have a party:)"},
-    {id: 4, message: "Can you help me with code review today?"},
-    {id: 5, message: "Are you going to gim today?"},
-]
+    profilePage: {
+        posts: [
+            {id: 1, message: "How are you?", likesCount: 19},
+            {id: 2, message: "Where have you been?", likesCount: 3},
+            {id: 3, message: "Pics from our party yesterday", likesCount: 8},
+            {id: 4, message: "How important is to be proactive?", likesCount: 27},
+            {id: 5, message: "What are your goals for today, for this week?", likesCount: 54},
+        ],
+    },
+    messagesPage: {
+        dialogs: [
+            {id: 1, name: "Dimych"},
+            {id: 2, name: "Victor"},
+            {id: 3, name: "Bob"},
+            {id: 4, name: "John"},
+            {id: 5, name: "Anna"},
+            {id: 6, name: "Dennis"},
+        ],
+        messages: [
+            {id: 1, message: "How are you?"},
+            {id: 2, message: "Are you busy now?"},
+            {id: 3, message: "Today I have a party:)"},
+            {id: 4, message: "Can you help me with code review today?"},
+            {id: 5, message: "Are you going to gim today?"},
+        ]
+    },
 }
 
 export default state
