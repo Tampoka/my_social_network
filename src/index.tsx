7a66572125e4a0c80ecf4206ed070153f16ab4de
Tampoka <goal.thanks@gmail.com>
2021-08-07 21:40:14 +0900

    moved all data to state.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,28 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import DialogItem from "./components/Dialogs/DialogItem/DialogItem";
 
-let posts = [
-    {id: 1, message: "How are you?", likesCount: 19},
-    {id: 2, message: "Where have you been?", likesCount: 3},
-    {id: 3, message: "Pics from our party yesterday", likesCount: 8},
-    {id: 4, message: "How important is to be proactive?", likesCount: 27},
-    {id: 5, message: "What are your goals for today, for this week?", likesCount: 54},
-]
-let dialogs = [
-    {id: 1, name: "Dimych"},
-    {id: 2, name: "Victor"},
-    {id: 3, name: "Bob"},
-    {id: 4, name: "John"},
-    {id: 5, name: "Anna"},
-    {id: 6, name: "Dennis"},
-]
-let messages = [
-    {id: 1, message: "How are you?"},
-    {id: 2, message: "Are you busy now?"},
-    {id: 3, message: "Today I have a party:)"},
-    {id: 4, message: "Can you help me with code review today?"},
-    {id: 5, message: "Are you going to gim today?"},
-]
 ReactDOM.render(
     <App posts={posts} dialogs={dialogs} messages={messages}/>,
   document.getElementById('root')
