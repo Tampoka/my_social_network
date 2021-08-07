4f02906ac7b66a0f15bc9c215a4919ff241727e8
Tampoka <goal.thanks@gmail.com>
2021-08-07 17:47:05 +0900

    moved data with posts up to Profile

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-    let posts = [
-        {id: 1, message: "How are you?", likesCount: 19},
-        {id: 2, message: "Where have you been?", likesCount: 3},
-        {id: 3, message: "Pics from our party yesterday", likesCount: 8},
-        {id: 4, message: "How important is to be proactive?", likesCount: 27},
-        {id: 5, message: "What are your goals for today, for this week?", likesCount: 54},
-    ]
-    let postElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+type MyPostsPropsType= {
+    posts:Array<PostPropsType>
+}
+type PostPropsType={
+    id:number
+    message:string
+    likesCount:number
+}
+const MyPosts = (props:MyPostsPropsType) => {
+    let postElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
     return (
         <div className={s.postsBlock}>
             <h3>My Posts</h3>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,9 +4,16 @@ import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile=()=> {
+      let posts = [
+       {id: 1, message: "How are you?", likesCount: 19},
+       {id: 2, message: "Where have you been?", likesCount: 3},
+       {id: 3, message: "Pics from our party yesterday", likesCount: 8},
+       {id: 4, message: "How important is to be proactive?", likesCount: 27},
+       {id: 5, message: "What are your goals for today, for this week?", likesCount: 54},
+   ]
     return  <div>
        <ProfileInfo/>
-       <MyPosts/>
+       <MyPosts posts={posts}/>
     </div>
     }
 
