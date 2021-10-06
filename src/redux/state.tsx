74da2d7d5bdd78d52aafbfbc3ace29973d956d77
Tampoka <goal.thanks@gmail.com>
2021-10-06 13:01:45 +0900

    changed img

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -28,7 +28,7 @@ let state = {
     sideBar:{
         friends:[
             {
-                id:1, name:"Bob",img: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=386&q=80",isOnline:true
+                id:1, name:"Bob",img: "https://images.unsplash.com/photo-1600603955656-e3c9e1ad7f64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",isOnline:true
             },
             {
                 id:2, name:"Kate",img: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",isOnline:false
