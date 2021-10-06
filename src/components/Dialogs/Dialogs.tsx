c11536dbd579b16bcf96d9e0bf9d672ac153254a
Tampoka <goal.thanks@gmail.com>
2021-10-06 17:15:11 +0900

    added ref + callback for "send" btn

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -23,6 +23,9 @@ const Dialogs = (props: DialogsPropsType) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
 
+    const newMessageElement = React.createRef<HTMLTextAreaElement>()
+    const addMessage = () => alert(newMessageElement.current?.value)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
@@ -31,8 +34,9 @@ const Dialogs = (props: DialogsPropsType) => {
             <div className={s.messages}>
                 {messagesElements}
                 <div className={s.sendMessage}>
-                    <textarea placeholder={"Write a message"}></textarea>
-                    <button>Send message</button>
+                    <textarea placeholder={"Write a message"}
+                              ref={newMessageElement}></textarea>
+                    <button onClick={addMessage}>Send message</button>
                 </div>
             </div>
         </div>
