48449858a9c61ae422976b0fbb2e4935d52093b3
Tampoka <goal.thanks@gmail.com>
2021-10-06 16:57:24 +0900

    added React.createRef +type for textarea

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,25 +2,31 @@ import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-type MyPostsPropsType= {
-    posts:Array<PostPropsType>
+type MyPostsPropsType = {
+    posts: Array<PostPropsType>
 }
-type PostPropsType={
-    id:number
-    message:string
-    likesCount:number
+type PostPropsType = {
+    id: number
+    message: string
+    likesCount: number
 }
-const MyPosts = (props:MyPostsPropsType) => {
+const MyPosts = (props: MyPostsPropsType) => {
     let postElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+    const onAddPost = () => {
+        alert(newPostElement.current?.value)
+
+
+    }
     return (
         <div className={s.postsBlock}>
             <h3>My Posts</h3>
             <div>
                 <div>
-                    <textarea placeholder={'Enter your thoughts'}></textarea>
+                    <textarea placeholder='Enter your thoughts' ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add Post</button>
+                    <button onClick={onAddPost}>Add Post</button>
                 </div>
             </div>
             <div className={s.posts}>
