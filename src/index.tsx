0d300ced87ced0fe280dcf4f3838cc42b3e51fc3
Tampoka <goal.thanks@gmail.com>
2021-10-07 19:48:02 +0900

    got rerenderEntireTree to work

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {rerenderEntireTree} from "./render";
 import state from "./redux/state";
-import {addPost} from "./redux/state";
 
-let rerenderEntireTree=()=>{
-    ReactDOM.render(
-        <BrowserRouter>
-            <App state={state} addPost={addPost}/>
-        </BrowserRouter>,
-        document.getElementById('root')
-
-    );
-}
-rerenderEntireTree()
+rerenderEntireTree(state)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
