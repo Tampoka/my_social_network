1bf451cc3b2d4ba5a665ea57786020e84e328a59
Tampoka <goal.thanks@gmail.com>
2021-10-07 20:51:19 +0900

    got rid of unnecessary imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './index.css';
+
 import reportWebVitals from './reportWebVitals';
 import {rerenderEntireTree} from "./render";
 import state from "./redux/state";
