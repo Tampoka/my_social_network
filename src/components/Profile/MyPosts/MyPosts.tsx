8113e2581281d1ebb14221324ba4276e263be36a
Tampoka <goal.thanks@gmail.com>
2021-10-07 22:24:58 +0900

    added updateNewPostText in props, added onPostTextChange

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,26 +1,34 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
-import {PostPType} from "../../../redux/state";
+import {PostPType, updateNewPostText} from "../../../redux/state";
 
 type MyPostsPropsType = {
     posts: PostPType[]
-    addPostCallback:(postMessage:string)=>void
+    newPostText:string
+    addPostCallback: (postMessage: string) => void
+    updateNewPostText:(newText:string)=>void
 }
 
-const MyPosts :React.FC<MyPostsPropsType>= (props ) => {
+const MyPosts: React.FC<MyPostsPropsType> = (props) => {
     let postElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
     let newPostElement = React.createRef<HTMLTextAreaElement>()
     const onAddPost = () => {
-        props.addPostCallback(newPostElement.current?newPostElement.current.value:"")
-        newPostElement.current!.value=""
+        props.addPostCallback(newPostElement.current ? newPostElement.current.value : "")
+        newPostElement.current!.value = ""
+    }
+    const onPostTextChange = () => {
+        props.updateNewPostText(newPostElement.current!.value)
     }
     return (
         <div className={s.postsBlock}>
             <h3>My Posts</h3>
             <div>
                 <div>
-                    <textarea placeholder='Enter your thoughts' ref={newPostElement}></textarea>
+                    <textarea placeholder='Enter your thoughts'
+                              ref={newPostElement}
+                              value={props.newPostText}
+                              onChange={onPostTextChange}/>
                 </div>
                 <div>
                     <button onClick={onAddPost}>Add Post</button>
