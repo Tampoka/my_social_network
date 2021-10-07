485d1b02e3df0a8b9755d0de50a5d9b425457aad
Tampoka <goal.thanks@gmail.com>
2021-10-07 22:23:43 +0900

    added updateNewPostText

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,16 +3,21 @@ import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import {ProfilePageType} from "../../redux/state";
 
-type ProfilePropsType={
-    state:ProfilePageType
-    addPost:(postMessage:string)=>void
+type ProfilePropsType = {
+    profilePage: ProfilePageType
+    addPost: (postMessage: string) => void
+    updateNewPostText: (newText: string) => void
 }
 
-const Profile:React.FC<ProfilePropsType>=(props)=> {
-    return  <div>
-       <ProfileInfo/>
-       <MyPosts posts={props.state.posts} addPostCallback={props.addPost}/>
+const Profile: React.FC<ProfilePropsType> = (props) => {
+    return <div>
+        <ProfileInfo/>
+        <MyPosts posts={props.profilePage.posts}
+                 addPostCallback={props.addPost}
+                 newPostText={props.profilePage.newPostText}
+                 updateNewPostText={props.updateNewPostText}
+        />
     </div>
-    }
+}
 
 export default Profile
