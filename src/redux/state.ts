cb77c9e1c669806d1f06be8b7167ee421538b3a4
Tampoka <goal.thanks@gmail.com>
2021-10-08 13:14:54 +0900

    created store

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,126 +1,137 @@
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "How are you?", likesCount: 19},
+                {id: 2, message: "Where have you been?", likesCount: 3},
+                {id: 3, message: "Pics from our party yesterday", likesCount: 8},
+                {id: 4, message: "How important is to be proactive?", likesCount: 27},
+                {id: 5, message: "What are your goals for today, for this week?", likesCount: 54},
+            ],
+            newPostText: "Hello!"
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Dimych"},
+                {id: 2, name: "Victor"},
+                {id: 3, name: "Bob"},
+                {id: 4, name: "John"},
+                {id: 5, name: "Anna"},
+                {id: 6, name: "Dennis"},
+            ],
+            messages: [
+                {id: 1, message: "How are you?"},
+                {id: 2, message: "Are you busy now?"},
+                {id: 3, message: "Today I have a party:)"},
+                {id: 4, message: "Can you help me with code review today?"},
+                {id: 5, message: "Are you going to gim today?"},
+            ],
+            newMessageText: "How have you been?"
+        },
+        sidebar: {
+            friends: [
+                {
+                    id: 1,
+                    name: "Bob",
+                    img: "https://images.unsplash.com/photo-1600603955656-e3c9e1ad7f64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
+                    isOnline: true
+                },
+                {
+                    id: 2,
+                    name: "Kate",
+                    img: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
+                    isOnline: false
+                },
+                {
+                    id: 3,
+                    name: "John",
+                    img: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
+                    isOnline: true
+                },
 
-let onChange=()=>{
-    console.log("state changed")
-}
-
-export type PostPType ={
-    id:number
-    message:string
-    likesCount:number
-}
-export type DialogType ={
-    id:number
-    name:string
-}
-export type MessageType ={
-    id:number
-    message:string
-}
-export type FriendType ={
-    id:number
-    name:string
-    img:string
-    isOnline:boolean
-}
-
-export type ProfilePageType ={
-    posts:PostPType[]
-    newPostText:string
-}
-export type DialogsPageType ={
-    dialogs:DialogType[]
-    messages:MessageType[]
-    newMessageText:string
-}
-
-export type SidebarType ={
-    friends:FriendType[]
-}
-
-export type RootStateType ={
-    profilePage:ProfilePageType
-    dialogsPage:DialogsPageType
-    sidebar:SidebarType
-}
-let state:RootStateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: "How are you?", likesCount: 19},
-            {id: 2, message: "Where have you been?", likesCount: 3},
-            {id: 3, message: "Pics from our party yesterday", likesCount: 8},
-            {id: 4, message: "How important is to be proactive?", likesCount: 27},
-            {id: 5, message: "What are your goals for today, for this week?", likesCount: 54},
-        ],
-        newPostText:"Hello!"
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: "Dimych"},
-            {id: 2, name: "Victor"},
-            {id: 3, name: "Bob"},
-            {id: 4, name: "John"},
-            {id: 5, name: "Anna"},
-            {id: 6, name: "Dennis"},
-        ],
-        messages: [
-            {id: 1, message: "How are you?"},
-            {id: 2, message: "Are you busy now?"},
-            {id: 3, message: "Today I have a party:)"},
-            {id: 4, message: "Can you help me with code review today?"},
-            {id: 5, message: "Are you going to gim today?"},
-        ],
-        newMessageText:"How have you been?"
+    getState(){
+        return this._state
     },
-    sidebar:{
-        friends:[
-            {
-                id:1, name:"Bob",img: "https://images.unsplash.com/photo-1600603955656-e3c9e1ad7f64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",isOnline:true
-            },
-            {
-                id:2, name:"Kate",img: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",isOnline:false
-            },
-            {
-                id:3, name:"John",img: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",isOnline:true
-            },
-
-        ]
+    onChange() {
+        console.log("state changed")
+    },
+    addPost() {
+        const newPost: PostPType = {
+            id: 6,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ""
+        this.onChange()
+    },
+    updateNewPostText(newText: string) {
+        this._state.profilePage.newPostText = newText
+        this.onChange()
+    },
+    addMessage() {
+        const newMessage: MessageType = {
+            id: 6,
+            message: this._state.dialogsPage.newMessageText,
+        }
+        this._state.dialogsPage.messages.push(newMessage)
+        this._state.dialogsPage.newMessageText = ""
+        this.onChange()
+    },
+    updateNewMessageText(newText: string) {
+        this._state.dialogsPage.newMessageText = newText
+        this.onChange()
+    },
+    subscribe(observer: () => void) {
+        this.onChange = observer
     }
-}
 
-export const addPost=()=>{
-    const newPost:PostPType={
-        id:6,
-        message:state.profilePage.newPostText,
-        likesCount:0
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText=""
-    onChange()
-}
-
-export const updateNewPostText=(newText:string)=>{
-    state.profilePage.newPostText=newText
-    onChange()
-}
-
-export const addMessage=()=>{
-    const newMessage:MessageType={
-        id:6,
-        message:state.dialogsPage.newMessageText,
-    }
-    state.dialogsPage.messages.push(newMessage)
-    state.dialogsPage.newMessageText=""
-    onChange()
-}
-
-export const updateNewMessageText=(newText:string)=>{
-    state.dialogsPage.newMessageText=newText
-    onChange()
-}
-
-export  const subscribe = (observer:()=>void) => {
-onChange=observer
 }
 
 
-export default state
+export type PostPType = {
+    id: number
+    message: string
+    likesCount: number
+}
+export type DialogType = {
+    id: number
+    name: string
+}
+export type MessageType = {
+    id: number
+    message: string
+}
+export type FriendType = {
+    id: number
+    name: string
+    img: string
+    isOnline: boolean
+}
+
+export type ProfilePageType = {
+    posts: PostPType[]
+    newPostText: string
+}
+export type DialogsPageType = {
+    dialogs: DialogType[]
+    messages: MessageType[]
+    newMessageText: string
+}
+
+export type SidebarType = {
+    friends: FriendType[]
+}
+
+export type RootStateType = {
+    profilePage: ProfilePageType
+    dialogsPage: DialogsPageType
+    sidebar: SidebarType
+}
+
+
+export default store
+// window.store=store
