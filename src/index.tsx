3be517fb3cad9f9ed11e8709c2b6cf86d1afbf17
Tampoka <goal.thanks@gmail.com>
2021-10-08 11:43:25 +0900

    removed parameter "state" from rerenderEntireTree

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import React from "react";
 
-let rerenderEntireTree = (state: RootStateType) => {
+let rerenderEntireTree = () => {
     ReactDOM.render(
         <BrowserRouter>
             <App state={state}
@@ -19,7 +19,7 @@ let rerenderEntireTree = (state: RootStateType) => {
         document.getElementById('root')
     )
 }
-rerenderEntireTree(state)
+rerenderEntireTree()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
