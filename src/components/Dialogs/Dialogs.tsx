0b38f13da8aa8fd0e069f51da51a915816b940a2
Tampoka <goal.thanks@gmail.com>
2021-10-08 10:06:47 +0900

    wrapped textarea, button in divs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,10 +4,10 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import {DialogsPageType} from "../../redux/state";
 
-type DialogsPropsType={
-    state:DialogsPageType
+type DialogsPropsType = {
+    state: DialogsPageType
 }
-const Dialogs:React.FC<DialogsPropsType> = (props) => {
+const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
@@ -23,9 +23,13 @@ const Dialogs:React.FC<DialogsPropsType> = (props) => {
             <div className={s.messages}>
                 {messagesElements}
                 <div className={s.sendMessage}>
+                    <div>
                     <textarea placeholder={"Write a message"}
-                              ref={newMessageElement}></textarea>
-                    <button onClick={addMessage}>Send message</button>
+                              ref={newMessageElement}/>
+                    </div>
+                    <div>
+                        <button onClick={addMessage}>Send</button>
+                    </div>
                 </div>
             </div>
         </div>
