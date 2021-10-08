f29149247a246a2c77f03384a97ce3a17cf811c4
Tampoka <goal.thanks@gmail.com>
2021-10-08 11:20:09 +0900

    got rid of Ref

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
@@ -14,10 +14,9 @@ const Dialogs: React.FC<DialogsPropsType> = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
 
-    const newMessageElement = React.createRef<HTMLTextAreaElement>()
     const onAddMessage = () => props.addMessage()
-    const onMessageTextChange=()=>{
-        props.updateNewMessageText(newMessageElement.current!.value)
+    const onMessageTextChange=(e:ChangeEvent<HTMLTextAreaElement>)=>{
+        props.updateNewMessageText(e.currentTarget.value)
     }
 
     return (
@@ -30,8 +29,8 @@ const Dialogs: React.FC<DialogsPropsType> = (props) => {
                 <div className={s.sendMessage}>
                     <div>
                     <textarea placeholder={"Write a message"}
-                              ref={newMessageElement}
                               onChange={onMessageTextChange}
+                              value={props.state.newMessageText}
                     />
                     </div>
                     <div>
