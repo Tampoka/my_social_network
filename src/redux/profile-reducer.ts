86eed688843f84d4a493d11f9e37105addbf8438
Tampoka <goal.thanks@gmail.com>
2021-10-09 18:03:22 +0900

    created profileReducer

---
+++ src/redux/profile-reducer.ts
@@ -0,0 +1,20 @@
+import {AddPostActionType, PostType, ProfilePageType, UpdateNewPostActionType} from "./state";
+
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
+export const profileReducer=(state:ProfilePageType, action:AddPostActionType|UpdateNewPostActionType)=>{
+    if (action.type === ADD_POST) {
+        const newPost: PostType = {
+            id: 6,
+            message: state.newPostText,
+            likesCount: 0
+        }
+       state.posts.push(newPost)
+        state.newPostText = ""
+
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+        state.newPostText = action.newText
+    }
+    return state
+}
