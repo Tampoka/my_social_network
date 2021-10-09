c09c4aa043b734363fad0f60ffd671ac3ce75965
Tampoka <goal.thanks@gmail.com>
2021-10-09 18:14:53 +0900

    created dialogsReducer

---
+++ src/redux/dialogs-reducer.ts
@@ -0,0 +1,19 @@
+import {AddMessageActionType, DialogsPageType, MessageType, UpdateNewMessageActionType} from "./state";
+
+const ADD_MESSAGE = "ADD-MESSAGE";
+const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
+
+export const dialogsReducer=(state:DialogsPageType, action:AddMessageActionType|UpdateNewMessageActionType)=>{
+if (action.type === ADD_MESSAGE) {
+        const newMessage: MessageType = {
+            id: 6,
+            message: state.newMessageText,
+        }
+        state.messages.push(newMessage)
+        state.newMessageText = ""
+
+    } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+        state.newMessageText = action.newText
+    }
+    return state
+}
