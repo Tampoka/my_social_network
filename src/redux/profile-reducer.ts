713643bc592f10a0fbdcfad9f0b86c743efd5067
Tampoka <goal.thanks@gmail.com>
2021-10-09 21:09:57 +0900

    refactored to switch

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -4,19 +4,22 @@ const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 const profileReducer=(state:ProfilePageType, action:AddPostActionType|UpdateNewPostActionType)=>{
-    if (action.type === ADD_POST) {
-        const newPost: PostType = {
-            id: 6,
-            message: state.newPostText,
-            likesCount: 0
-        }
-       state.posts.push(newPost)
-        state.newPostText = ""
-
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText
+    switch (action.type) {
+        case ADD_POST:
+            const newPost: PostType = {
+                id: 6,
+                message: state.newPostText,
+                likesCount: 0
+            }
+            state.posts.push(newPost)
+            state.newPostText = ""
+            return state
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText
+            return state
+        default:
+            return state
     }
-    return state
 }
 
 export default profileReducer
