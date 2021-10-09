a3ef3f15d5f517d469ea172a5965521fed0737e5
Tampoka <goal.thanks@gmail.com>
2021-10-09 21:10:32 +0900

    refactored to switch

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -4,18 +4,21 @@ const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
 const dialogsReducer=(state:DialogsPageType, action:AddMessageActionType|UpdateNewMessageActionType)=>{
-if (action.type === ADD_MESSAGE) {
-        const newMessage: MessageType = {
-            id: 6,
-            message: state.newMessageText,
-        }
-        state.messages.push(newMessage)
-        state.newMessageText = ""
-
-    } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageText = action.newText
+    switch (action.type) {
+        case ADD_MESSAGE:
+            const newMessage: MessageType = {
+                id: 6,
+                message: state.newMessageText,
+            }
+            state.messages.push(newMessage)
+            state.newMessageText = ""
+            return state
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newText
+            return state
+        default:
+            return state
     }
-    return state
 }
 
 export  default dialogsReducer
