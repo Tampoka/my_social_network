3dd0e0cbc46d072ca35f44ad192d908a180386d1
Tampoka <goal.thanks@gmail.com>
2021-10-10 19:34:41 +0900

    made store:any to fix later

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,9 @@ import {Redirect, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import StoreType from "./redux/redux-store";
 
 type AppPropsType = {
-  store:StoreType
+  store:any
 }
 const App: React.FC<AppPropsType> = (props) => {
     const state=props.store.getState()
