b74cc5dd6968b6dcc7b8ea01b200d75f1904c06d
Tampoka <goal.thanks@gmail.com>
2021-10-10 20:03:38 +0900

    changed to accept callbacks in props from Container

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,26 +2,25 @@ import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import {addMessageActionCreator, updateNewMessageTextActionCreator} from "../../redux/dialogs-reducer";
-import {DialogsPageType} from "../../redux/store";
-import {ActionsType} from "../../redux/profile-reducer";
+import {DialogType, MessageType} from "../../redux/store";
 
 
 type DialogsPropsType = {
-    state: DialogsPageType
-    dispatch: (action:ActionsType)=>void
+    dialogs:DialogType[]
+    messages:MessageType[]
+    newMessageText:string
+    addMessage:()=>void
+    updateNewMessageText:(text:string)=>void
 }
 const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
-    let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
-    let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = props.messages.map(m => <Message message={m.message}/>)
 
-    const onAddMessage = () => {
-        props.dispatch(addMessageActionCreator())
-    }
+    const onAddMessage = () => props.addMessage()
     const onMessageTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        let text= e.currentTarget.value
-        props.dispatch(updateNewMessageTextActionCreator(text))
+        const text= e.currentTarget.value
+        props.updateNewMessageText(text)
     }
 
     return (
@@ -35,7 +34,7 @@ const Dialogs: React.FC<DialogsPropsType> = (props) => {
                     <div>
                     <textarea placeholder={"Write a message"}
                               onChange={onMessageTextChange}
-                              value={props.state.newMessageText}
+                              value={props.newMessageText}
                     />
                     </div>
                     <div>
