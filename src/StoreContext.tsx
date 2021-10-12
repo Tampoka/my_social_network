8f20aa8a4d2343ca526276542151c10ee9b90aa5
Tampoka <goal.thanks@gmail.com>
2021-10-12 12:54:11 +0900

    created Provider

---
+++ src/StoreContext.tsx
@@ -0,0 +1,12 @@
+import React, {ProviderProps} from "react";
+import store from "./redux/redux-store";
+
+const StoreContext=React.createContext(store);
+
+export const Provider=(props:ProviderProps<any>)=>{
+    return   <StoreContext.Provider value={store}>
+    {props.children}
+    </StoreContext.Provider>
+}
+
+export default StoreContext
