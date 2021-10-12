d7db87cd0114375643a30700b161491e2e7bc75d
Tampoka <goal.thanks@gmail.com>
2021-10-12 12:17:03 +0900

    passing data to presentational MyPosts

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,30 +1,26 @@
 import React from "react";
-import {StoreType} from "../../../redux/store";
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import StoreContext from "../../../StoreContext";
 
 type MyPostsContainerPropsType = {
-   store:StoreType
 }
 
 const MyPostsContainer: React.FC<MyPostsContainerPropsType> = (props) => {
-
-        const state=props.store.getState()
-
-    const onAddPost = () => props.store.dispatch(addPostActionCreator())
-
-    const onPostTextChange = (text: string) => {
-        props.store.dispatch(updateNewPostTextActionCreator(text))
+    return <StoreContext.Consumer>
+        {
+            (store) => {
+            const state = store.getState()
+            const onAddPost = () => store.dispatch(addPostActionCreator())
+            const onPostTextChange = (text: string) => {
+            store.dispatch(updateNewPostTextActionCreator(text))
+        }
+            return <MyPosts updateNewPostText={onPostTextChange}
+            addPost={onAddPost}
+            posts={state.profilePage.posts}
+            newPostText={state.profilePage.newPostText}/>
+        }
+        }
+            </StoreContext.Consumer>
     }
-
-    return (
-    <StoreContext.Consumer>
-   <MyPosts updateNewPostText={onPostTextChange}
-                    addPost={onAddPost}
-                    posts={state.profilePage.posts}
-                    newPostText={state.profilePage.newPostText}/>
-    </StoreContext.Consumer>
-)}
-
-export default MyPostsContainer
+            export default MyPostsContainer
