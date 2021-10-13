de2d5799c648acf39b07ef06632a9451ecaea9e4
Tampoka <goal.thanks@gmail.com>
2021-10-13 10:52:54 +0900

    added CopycomposeEnhancers to use redux-devtools-extension

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,6 +2,7 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 let rootReducer=combineReducers({
     profilePage:profileReducer,
@@ -10,8 +11,10 @@ let rootReducer=combineReducers({
 })
 
 export type AppStateType=ReturnType<typeof rootReducer>
+const composeEnhancers = composeWithDevTools
 
-let store=createStore(rootReducer)
+
+let store=createStore(rootReducer,  composeEnhancers())
 
 
 
