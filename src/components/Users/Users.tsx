a4f5b7495084f960cc681c8aa31b952abcacbeb7
Tampoka <goal.thanks@gmail.com>
2021-10-13 23:22:03 +0900

    calling setUsers to render more users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,7 +4,32 @@ import s from "./Users.module.css";
 
 
 const Users: React.FC<UsersPropsType> = (props: UsersPropsType) => {
-
+    {props.usersPage.users.length<=3&&props.setUsers([
+        {
+            id: 1,
+            fullName: "Kate",
+            photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5dMYMr1CCTycSQd2YQatl4bvzK5T90Renlw&usqp=CAU",
+            status: "I am looking for new job",
+            isFollowing: true,
+            location: {city: "New York", country: "USA"}
+        },
+        {
+            id: 2,
+            fullName: "John",
+            photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8wvbPOt0gK-5yGATP1Beo7Mkk7LT1M6KZLw&usqp=CAU",
+            status: "I am moved to new office",
+            isFollowing: true,
+            location: {city: "London", country: "Great Britain"}
+        },
+        {
+            id: 3,
+            fullName: "Bob",
+            photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbz8f-cdfPQtfH1EP3x1V2pMDyLpDMmuzKbg&usqp=CAU",
+            status: "Happiest ever",
+            isFollowing: false,
+            location: {city: "Melbourne", country: "Australia"}
+        },
+    ])}
     return <div>
         {
             props.usersPage.users.map(u => <div key={u.id} className={s.user}>
@@ -27,6 +52,7 @@ const Users: React.FC<UsersPropsType> = (props: UsersPropsType) => {
                 </div>
             </div>)
         }
+        <button>Show More</button>
     </div>
 }
 
