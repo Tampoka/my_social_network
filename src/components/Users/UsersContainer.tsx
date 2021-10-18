a33b416ed1fdb055a8f0c695eca2850db4f12320
Tampoka <goal.thanks@gmail.com>
2021-10-19 08:38:17 +0900

    added setCurrentPage to mapDispatchToProps

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,7 +1,14 @@
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {Dispatch} from "redux";
-import {followAC, InitialStateType, setUsersAC, unFollowAC, UserType} from "../../redux/users-reducer";
+import {
+    followAC,
+    InitialStateType,
+    setCurrentPageAC,
+    setUsersAC,
+    unFollowAC,
+    UserType
+} from "../../redux/users-reducer";
 import UsersC from "./Users";
 
 type MapStateToPropsType = {
@@ -14,6 +21,7 @@ type MapDispatchToPropsType = {
     follow: (userId: number) => void
     unFollow: (userId: number) => void
     setUsers: (users: UserType[]) => void
+    setCurrentPage:(pageNumber:number)=>void
 }
 
 export type UsersPropsType = MapStateToPropsType & MapDispatchToPropsType
@@ -37,6 +45,9 @@ let mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
         },
         setUsers: (users: UserType[]) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage:(pageNumber:number)=>{
+            dispatch(setCurrentPageAC(pageNumber))
         }
     }
 }
