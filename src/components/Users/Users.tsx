b2a7540ac2ff431cef11667b8f1f4231700f8421
Tampoka <goal.thanks@gmail.com>
2021-10-19 10:51:56 +0900

    getting totalUsersCount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -27,9 +27,12 @@ class Users extends React.Component<UsersPropsType, any> {
         axios.get<any>(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+
             })
     }
-    onPageChanged=(pageNumber:number)=>{
+
+    onPageChanged = (pageNumber: number) => {
         this.props.setCurrentPage(pageNumber)
         axios.get<any>(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
@@ -45,16 +48,16 @@ class Users extends React.Component<UsersPropsType, any> {
             pages.push(i)
         }
         return <div className={s.usersContainer}>
-
             <div className={s.users}>
                 {pages.map(p => {
-                   return <span className={this.props.currentPage === p? s.selectedPage:''}
-                   onClick={()=>this.onPageChanged(p)}>{p}</span>
+                    return <span className={this.props.currentPage === p ? s.selectedPage : ''}
+                                 onClick={() => this.onPageChanged(p)}>{p}</span>
                 })}
                 {
                     this.props.usersPage.users.map(u => <div key={u.id} className={s.user}>
                             <div className={s.avatar}>
-                                <div className={s.userPhoto}><img src={u.photos.small ?u.photos.small :userPhoto} alt="user"/></div>
+                                <div className={s.userPhoto}><img src={u.photos.small ? u.photos.small : userPhoto}
+                                                                  alt="user"/></div>
                                 <div>
                                     {u.isFollowing
                                         ? <button onClick={() => this.props.unFollow(u.id)}>UnFollow</button>
