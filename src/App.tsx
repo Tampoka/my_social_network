25d56d6b32c3c3a6584597a957f8e4077b9e8548
Tampoka <goal.thanks@gmail.com>
2021-10-24 23:31:12 +0900

    added Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import {Route} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -22,14 +22,14 @@ const App: React.FC<AppPropsType> = (props) => {
             <div className="app-wrapper-content">
                 <Route path="/dialogs" render={() =>
                     <DialogsContainer />}/>
-                <Route path="/profile/:userId" render={() =>
+                <Route path="/profile/:userId?" render={() =>
                     <ProfileContainer />}/>
                 <Route path="/users" render={() =>
                     <UsersContainer />}/>
                 <Route path="/news" component={News}/>
                 <Route path="/music" component={Music}/>
                 <Route path="/settings" component={Settings}/>
-                {/*<Redirect to="/profile"/>*/}
+                <Redirect to="/profile"/>
             </div>
         </div>
     );
