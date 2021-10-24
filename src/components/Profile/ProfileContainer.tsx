aa17c6110994f8f666f48740ab9cc88180206084
Tampoka <goal.thanks@gmail.com>
2021-10-24 15:41:59 +0900

    one more Container through connect

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Profile from "./Profile";
+import axios from "axios";
+import {connect} from "react-redux";
+import {AppStateType} from "../../redux/redux-store";
+import {InitialStateType, UserType} from "../../redux/users-reducer";
 
 
 class ProfileContainer extends React.Component<any, any> {
+    componentDidMount() {
+        axios.get<any>(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .then(response => {
+                this.props.setUserProfile(response.data.items)
+
+            })
+    }
+
     render() {
         return (
             <Profile {...this.props}/>
@@ -10,4 +22,13 @@ class ProfileContainer extends React.Component<any, any> {
     }
 }
 
-export default ProfileContainer
+
+
+
+let mapStateToProps=(state:AppStateType)=>{
+    a:13
+}
+
+export default connect (mapStateToProps,{
+    setUserProfile
+})(ProfileContainer)
