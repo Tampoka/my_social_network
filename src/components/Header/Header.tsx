b7caded1cdc742cc4d9ee365ab1ee7f3a59401e3
Tampoka <goal.thanks@gmail.com>
2021-10-25 22:00:23 +0900

    added Navlink to login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Header.module.css'
 
 const Header:React.FC=()=> {
     return <header className={s.header}>
         <img src='http://www.logobook.com/wp-content/uploads/2016/10/Community_Builders_and_Building_Materials_logo.svg' alt='logo'/>
 
-        <div className={s.loginBlock}>Login</div>
+        <div className={s.loginBlock}>
+            <NavLink to={'/login'}>Login</NavLink>
+        </div>
     </header>
     }
 
