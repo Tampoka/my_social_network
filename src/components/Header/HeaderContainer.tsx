0bd8b8b0ca5b8999b9b4cf88a8cee4aeba6fc941
Tampoka <goal.thanks@gmail.com>
2021-10-25 22:41:40 +0900

    passing withCredentials as second parameter in Get request

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import axios from "axios";
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
-
+        axios.get<any>(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
+            .then(response => {
+               debugger
+            })
     }
 
     render() {
