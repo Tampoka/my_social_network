0d1ae93f771269aef520298b70917d41706c8230
Tampoka <goal.thanks@gmail.com>
2021-10-25 22:29:43 +0900

    created HeaderContainer

---
+++ src/components/Header/HeaderContainer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+
+class HeaderContainer extends React.Component {
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return <Header/>
+    }
+}
+
+export default HeaderContainer
