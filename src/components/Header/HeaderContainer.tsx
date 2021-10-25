03cc231357d0bfe4a09f01b067353da2ab3c65e7
Tampoka <goal.thanks@gmail.com>
2021-10-25 22:54:05 +0900

    passing to connect()

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import axios from "axios";
+import {connect} from "react-redux";
+import {setAuthUserData} from "../../redux/auth-reducer";
+import {AppStateType} from "../../redux/redux-store";
+import {RouteComponentProps} from "react-router-dom";
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
         axios.get<any>(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
             .then(response => {
-               debugger
+                debugger
             })
     }
 
@@ -16,4 +20,14 @@ class HeaderContainer extends React.Component {
     }
 }
 
-export default HeaderContainer
+type MapStateToPropsType = {}
+type MapDispatchToPropsType = {
+    setAuthUserData: (userId: null | string, email: null | string, login: null | string) => void
+}
+let mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
+    profile: state.profilePage.profile
+})
+
+export type HeaderContainerPropsType = MapStateToPropsType & MapDispatchToPropsType
+
+export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer)
