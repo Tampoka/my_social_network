8667eefc0052b1b109178539daec39a3dcb74002
Tampoka <goal.thanks@gmail.com>
2021-10-25 21:55:43 +0900

    added login to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,8 @@ import s from './Header.module.css'
 const Header:React.FC=()=> {
     return <header className={s.header}>
         <img src='http://www.logobook.com/wp-content/uploads/2016/10/Community_Builders_and_Building_Materials_logo.svg' alt='logo'/>
+
+        <div className={s.loginBlock}>Login</div>
     </header>
     }
 
