705ac82e7c1c28f9313d2da4ee88799aa519c98e
Tampoka <goal.thanks@gmail.com>
2021-10-25 21:42:29 +0900

    added  AC + AType

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,7 +1,4 @@
-const FOLLOW = "FOLLOW";
-const UNFOLLOW = "UNFOLLOW";
-const TOGGLE_IS_FETCHING = "TOGGLE-IS-FETCHING"
-
+const SET_USER_DATA = "SET-USER-DATA";
 
 export type InitialStateType = {
     userId: null | string
@@ -15,68 +12,28 @@ const initialState = {
     email: null,
     login: null,
 }
-const usersReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case FOLLOW:
+        case SET_USER_DATA:
             return {
                 ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return {...u, isFollowing: true}
-                    }
-                    return u
-                })
+                ...action.data
             }
-        case UNFOLLOW:
-            return {
-                ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return {...u, isFollowing: false}
-                    }
-                    return u
-                })
-            }
-        case SET_USERS:
-            return {...state, users: action.users}
-        case SET_CURRENT_PAGE:
-            return {
-                ...state,
-                currentPage: action.pageNumber
-            }
-        case SET_TOTAL_USERS_COUNT:
-            return {...state, totalUsersCount: action.totalUsersCount}
-        case TOGGLE_IS_FETCHING:
-            return {...state, isFetching: action.isFetching}
         default:
             return state
     }
 }
 
 export  type ActionsType =
-    FollowActionType
-    | UnFollowActionType
-    | SetUsersActionType
-    | SetCurrentPageActionType
-    | SetTotalUsersCountActionType
-    | ToggleIsFetchingActionType
+    SetUserDataActionType
 
-export type FollowActionType = ReturnType<typeof follow>
-export type UnFollowActionType = ReturnType<typeof unFollow>
-export type SetUsersActionType = ReturnType<typeof setUsers>
-export type SetCurrentPageActionType = ReturnType<typeof setCurrentPage>
-export type SetTotalUsersCountActionType = ReturnType<typeof setTotalUsersCount>
-export type ToggleIsFetchingActionType = ReturnType<typeof toggleIsFetching>
 
-export const follow = (userId: number) => ({type: FOLLOW, userId} as const)
-export const unFollow = (userId: number) => ({type: UNFOLLOW, userId} as const)
-export const setUsers = (users: UserType[]) => ({type: SET_USERS, users} as const)
-export const setCurrentPage = (pageNumber: number) => ({type: SET_CURRENT_PAGE, pageNumber} as const)
-export const setTotalUsersCount = (totalUsersCount: number) => ({
-    type: SET_TOTAL_USERS_COUNT,
-    totalUsersCount
+export type SetUserDataActionType = ReturnType<typeof setUserData>
+
+export const setUserData = (userId:null | string, email:null | string, login:null | string) => ({
+    type: SET_USER_DATA,
+    data:{userId, email, login}
 } as const)
-export const toggleIsFetching = (isFetching: boolean) => ({type: TOGGLE_IS_FETCHING, isFetching} as const)
 
 
-export default usersReducer
+export default authReducer
