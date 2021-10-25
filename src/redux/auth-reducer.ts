e1e39b7fc398c6f47157ace9b22b8169120cedc0
Tampoka <goal.thanks@gmail.com>
2021-10-25 21:18:30 +0900

    creating auth-reducer

---
+++ src/redux/auth-reducer.ts
@@ -0,0 +1,129 @@
+export type InitialStateType = {
+    users: UserType[]
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
+    isFetching: boolean
+}
+
+export type UserType = {
+    id: number
+    name: string
+    photos: {
+        small: string
+        large: string
+    }
+    status: string
+    isFollowing: boolean
+    location: LocationType
+}
+
+export type LocationType = {
+    city: string
+    country: string
+}
+
+const FOLLOW = "FOLLOW";
+const UNFOLLOW = "UNFOLLOW";
+const SET_USERS = "SET-USERS";
+const SET_CURRENT_PAGE = "SET-CURRENT-PAGE";
+const SET_TOTAL_USERS_COUNT = "SET-TOTAL-USERS-COUNT"
+const TOGGLE_IS_FETCHING = "TOGGLE-IS-FETCHING"
+
+const initialState = {
+    users: [
+        // {
+        //     id: 1,
+        //     fullName: "Kate",
+        //     photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5dMYMr1CCTycSQd2YQatl4bvzK5T90Renlw&usqp=CAU",
+        //     status: "I am looking for new job",
+        //     isFollowing: true,
+        //     location: {city: "New York", country: "USA"}
+        // },
+        // {
+        //     id: 2,
+        //     fullName: "John",
+        //     photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8wvbPOt0gK-5yGATP1Beo7Mkk7LT1M6KZLw&usqp=CAU",
+        //     status: "I am moved to new office",
+        //     isFollowing: true,
+        //     location: {city: "London", country: "Great Britain"}
+        // },
+        // {
+        //     id: 3,
+        //     fullName: "Bob",
+        //     photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbz8f-cdfPQtfH1EP3x1V2pMDyLpDMmuzKbg&usqp=CAU",
+        //     status: "Happiest ever",
+        //     isFollowing: false,
+        //     location: {city: "Melbourne", country: "Australia"}
+        // },
+    ],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
+    isFetching: false
+}
+const usersReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, isFollowing: true}
+                    }
+                    return u
+                })
+            }
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, isFollowing: false}
+                    }
+                    return u
+                })
+            }
+        case SET_USERS:
+            return {...state, users: action.users}
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.pageNumber
+            }
+        case SET_TOTAL_USERS_COUNT:
+            return {...state, totalUsersCount: action.totalUsersCount}
+        case TOGGLE_IS_FETCHING:
+            return {...state, isFetching: action.isFetching}
+        default:
+            return state
+    }
+}
+
+export  type ActionsType =
+    FollowActionType
+    | UnFollowActionType
+    | SetUsersActionType
+    | SetCurrentPageActionType
+    | SetTotalUsersCountActionType
+    | ToggleIsFetchingActionType
+
+export type FollowActionType = ReturnType<typeof follow>
+export type UnFollowActionType = ReturnType<typeof unFollow>
+export type SetUsersActionType = ReturnType<typeof setUsers>
+export type SetCurrentPageActionType = ReturnType<typeof setCurrentPage>
+export type SetTotalUsersCountActionType = ReturnType<typeof setTotalUsersCount>
+export type ToggleIsFetchingActionType = ReturnType<typeof toggleIsFetching>
+
+export const follow = (userId: number) => ({type: FOLLOW, userId} as const)
+export const unFollow = (userId: number) => ({type: UNFOLLOW, userId} as const)
+export const setUsers = (users: UserType[]) => ({type: SET_USERS, users} as const)
+export const setCurrentPage = (pageNumber: number) => ({type: SET_CURRENT_PAGE, pageNumber} as const)
+export const setTotalUsersCount = (totalUsersCount: number) => ({
+    type: SET_TOTAL_USERS_COUNT,
+    totalUsersCount
+} as const)
+export const toggleIsFetching = (isFetching: boolean) => ({type: TOGGLE_IS_FETCHING, isFetching} as const)
+
+
+export default usersReducer
