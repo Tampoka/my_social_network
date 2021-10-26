02c921714aeb23a66edc3733b7d25ec351ea2409
Tampoka <goal.thanks@gmail.com>
2021-10-26 18:46:52 +0900

    created axios instance using axios.create

--- src/api/api.ts
+++ src/api/api.ts
@@ -18,11 +18,15 @@ export type ResponseUserType = {
     uniqueUrlName: string | null
 }
 
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "b22dc3a8-54db-4578-99ea-53ac8029e06a"
+    }
+})
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get<GetUserResponseType, any>(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-        {
-            withCredentials: true
-        })
-        .then(response=>response.data)
+    return instance.get<GetUserResponseType, any>(`users?page=${currentPage}&count=${pageSize}`,)
+        .then(response => response.data)
 }
