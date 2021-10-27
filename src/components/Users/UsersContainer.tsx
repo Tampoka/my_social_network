d3fa3953c4d99aec90a038e345d09fb9730f25bc
Tampoka <goal.thanks@gmail.com>
2021-10-27 17:48:15 +0900

    added getUsersThunkCreator to componentDidMount(), onPageChanged

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,11 +1,13 @@
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {
-    follow, getUsersThunkCreator,
+    follow,
+    getUsersThunkCreator,
     InitialStateType,
     setCurrentPage,
     setTotalUsersCount,
-    setUsers, toggleFollowingProgress,
+    setUsers,
+    toggleFollowingProgress,
     toggleIsFetching,
     unFollow,
     UserType
@@ -13,32 +15,17 @@ import {
 import React from "react";
 import Users from "./Users";
 import Preloader from "../../common/Preloader/Preloader";
-import { usersAPI} from "../../api/api";
 
 
 class UsersContainer extends React.Component<UsersContainerPropsType> {
     componentDidMount() {
-       /* this.props.toggleIsFetching(true)
-        usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
-            .then(data => {
-                this.props.toggleIsFetching(false)
-                this.props.setUsers(data.items)
-                this.props.setTotalUsersCount(data.totalCount)
-
-            })*/
         this.props.getUsersThunkCreator(this.props.currentPage, this.props.pageSize)
 
     }
 
     onPageChanged = (pageNumber: number) => {
-        this.props.setCurrentPage(pageNumber)
-        this.props.toggleIsFetching(true)
+        this.props.getUsersThunkCreator(pageNumber, this.props.pageSize)
 
-        usersAPI.getUsers(pageNumber, this.props.pageSize)
-            .then(data => {
-                this.props.toggleIsFetching(false)
-                this.props.setUsers(data.items)
-            })
     }
 
     render() {
