3804fe9ba6e3fd00f11f05049c67694c059ce7a5
Tampoka <goal.thanks@gmail.com>
2021-10-28 22:59:38 +0900

    add state, conditional rendering

--- src/components/Profile/ProfileStatus.tsx
+++ src/components/Profile/ProfileStatus.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
 export type ProfileStatusPropTypes = {
-    status:string
+    status: string
 };
 
 class ProfileStatus extends React.Component<ProfileStatusPropTypes> {
+    state = {
+        editMode: false
+    }
+
     render() {
         return (
             <div>
-                <div>
-                    <span>{this.props.status}</span>
-                </div>
-                <div>
-                    <input value={this.props.status}></input>
-                </div>
+                {!this.state.editMode &&
+                    <div>
+                        <span>{this.props.status}</span>
+                    </div>}
+
+                {this.state.editMode &&
+                    <div>
+                        <input value={this.props.status}></input>
+                    </div>}
             </div>
         );
     }
