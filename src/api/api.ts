bbc621cf25adff9c68a44368ef5bf53ebbd13892
Tampoka <goal.thanks@gmail.com>
2021-11-08 23:03:54 +0900

    Add updateStatus to profileApi

--- src/api/api.ts
+++ src/api/api.ts
@@ -33,7 +33,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: "https://social-network.samuraijs.com/api/1.0/",
     headers: {
-        "API-KEY": "b22dc3a8-54db-4578-99ea-53ac8029e06a"
+        "API-KEY": "73fdf213-e363-4a31-ad92-85b5d437ac0f"
     }
 })
 
@@ -56,6 +56,12 @@ export const profileAPI = {
     getProfile(userId: number) {
         return instance.get<GetProfileResponseType>(`profile/${userId}`)
             .then(response=>response.data)
+    },
+    getStatus(userId:number){
+        return instance.get(`profile/status/${userId}`)
+    },
+    updateStatus(status:string){
+        return instance.put(`profile/status`,{status:status})
     }
 }
 
