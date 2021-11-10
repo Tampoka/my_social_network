e735ab89768a34e3b09e5b93c615f23dc689b085
Tampoka <goal.thanks@gmail.com>
2021-11-10 21:12:30 +0900

    Create custom Input

--- src/components/FormControls/FormControls.tsx
+++ src/components/FormControls/FormControls.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import s from './FormControls.module.css'
 
-// @ts-ignore
-export const Textarea = ({input, meta, ...props}) => {
+export const Textarea:React.FC<any> = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error
     return (
         <div className={s.formControl + " " + (hasError ? s.error : "")}>
-            <div><textarea {...input} {...props}/></div>
+            <div>
+                <textarea {...input} {...props}/>
+            </div>
+            {hasError && <span>{meta.error}</span>}
+        </div>
+    );
+};
+
+export const Input:React.FC<any> = ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={s.formControl + " " + (hasError ? s.error : "")}>
+            <div><input {...input} {...props}
+            className={s.input}/></div>
             {hasError && <span>{meta.error}</span>}
         </div>
     );
