7c5d572423aac7ee30eb9b998f6ea80cb16a6009
Tampoka <goal.thanks@gmail.com>
2021-11-11 18:10:42 +0900

    Add Container component to Nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,38 +2,45 @@ import React from "react";
 import s from "./Nav.module.css";
 import {NavLink} from "react-router-dom";
 import Friends from "../Friends/Friends";
-import {SidebarType} from "../../redux/sidebar-reducer";
+import {FriendType} from "../../redux/sidebar-reducer";
+import {connect} from "react-redux";
+import {AppStateType} from "../../redux/redux-store";
 
-type NavPropsType={
-  state:SidebarType
-}
-const Nav:React.FC<NavPropsType> = (props) => {
-  return <div className={s.navContainer}>
-    <nav className={s.nav}>
-      <ul>
-        <li className={s.item}>
-          <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink to="/users" activeClassName={s.active}>Users</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink to="/news" activeClassName={s.active}>News</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
-        </li>
-      </ul>
-    </nav>
-    <Friends state={props.state}/>
-      </div>
+
+const Nav: React.FC<MapStateToPropsType> = (props) => {
+    return <div className={s.navContainer}>
+        <nav className={s.nav}>
+            <ul>
+                <li className={s.item}>
+                    <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/users" activeClassName={s.active}>Users</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+                </li>
+            </ul>
+        </nav>
+        <Friends friends={props.friends}/>
+    </div>
 
 };
+type MapStateToPropsType = {
+    friends: FriendType[]
+}
 
-export default Nav;
+let mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
+    friends: state.sidebar.friends
+})
+
+export default connect(mapStateToProps, {})(Nav);
