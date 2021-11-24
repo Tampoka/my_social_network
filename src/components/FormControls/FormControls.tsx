db31a104aaf3f92ed75084ad6053c9d242b2c320
Tampoka <goal.thanks@gmail.com>
2021-11-24 12:39:47 +0900

    Add createField.

--- src/components/FormControls/FormControls.tsx
+++ src/components/FormControls/FormControls.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './FormControls.module.css'
+import {Field} from "redux-form";
 
-type FormControlsPropsType={
-    input:any
-    meta:any
-    children:any
+type FormControlsPropsType = {
+    input: any
+    meta: any
+    children: any
 }
 const FormControl: React.FC<FormControlsPropsType> = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error
@@ -18,18 +19,29 @@ const FormControl: React.FC<FormControlsPropsType> = ({input, meta, ...props}) =
     )
 }
 
-export const Textarea: React.FC<any> = (props:any) => {
-    const {input,meta,...restProps}=props
+export const Textarea: React.FC<any> = (props: any) => {
+    const {input, meta, ...restProps} = props
     return <FormControl {...props}>
         <textarea {...input} {...restProps}/>
     </FormControl>
 };
 
-export const Input: React.FC<any> = (props:any) => {
-    const {input,meta,...restProps}=props
+export const Input: React.FC<any> = (props: any) => {
+    const {input, meta, ...restProps} = props
     return <FormControl {...props}>
         <input {...input} {...restProps} className={s.input}/>
     </FormControl>
 };
+
+
+export const createField = (name: string, component: any, validators: Array<any>, placeholder: string = "", props: {}, text: string = "", wrapperClass: any, fieldClass: any) =>
+    <div className={wrapperClass}>
+        <Field placeholder={placeholder}
+               name={name}
+               component={component}
+               validate={validators}
+               {...props}
+               className={fieldClass}/>
+    </div>
 
 
