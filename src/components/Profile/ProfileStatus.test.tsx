8b55394206485918c5ff56328585ae20f49ab987
Tampoka <goal.thanks@gmail.com>
2021-11-25 20:58:22 +0900

    Add first test for ProfileStatus.

---
+++ src/components/Profile/ProfileStatus.test.tsx
@@ -0,0 +1,12 @@
+import {create} from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
+import {updateStatus} from "../../redux/profile-reducer";
+
+describe("ProfileStatusComponent", () => {
+    test("status from props should be in the state", () => {
+        const component = create(<ProfileStatus status={"test text"}
+                                                updateStatus={updateStatus}/>)
+        const instance = component.root
+        expect(instance.props.status).toBe("test text")
+    })
+})
