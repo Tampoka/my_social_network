fef54bdb22215652e2e47e2443940f74a43ab015
Tampoka <goal.thanks@gmail.com>
2022-08-22 23:56:00 +0900

    Add Message.

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -19,10 +19,12 @@ const Chat: FC = () => {
 }
 
 const Messages: FC = () => {
-
+    const messages: any = [1, 2, 3, 4]
     return (
         <div>
-            messages
+            {messages.map((m: any) => <Message/>)}
+            {messages.map((m: any) => <Message/>)}
+            {messages.map((m: any) => <Message/>)}
         </div>
     )
 }
@@ -35,6 +37,22 @@ const AddMessageForm: FC = () => {
             <div>
                 <button>Send</button>
             </div>
+        </div>
+    )
+}
+
+const Message: FC = () => {
+    const message = {
+        url: 'https://via.placeholder.com/30',
+        author: 'Tampoka',
+        text: 'Hello,friends!'
+    }
+    return (
+        <div>
+            <img src={message.url}/> <b>{message.author}</b>
+            <br/>
+            {message.text}
+            <hr/>
         </div>
     )
 }
